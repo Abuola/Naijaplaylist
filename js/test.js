1611fc78ae9e324d00a823b6bfac700b827197aa
OLAITAN OYEDELE ABUBAKAR <lonerscode@gmail.com>
2022-10-11 14:23:38 +0100

    first commit

---
+++ js/test.js
@@ -0,0 +1,22 @@
+// const burger = document.querySelector(".burger");
+// console.log(burger);
+
+// const nav = document.querySelector("nav");
+// console.log(nav);
+
+// burger.addEventListener("click", function(){
+//     burger.classList.toggle("active");
+//     nav.classList.toggle("active");
+// })
+
+
+const burger = document.querySelector(".burger");
+console.log(burger);
+
+const nav = document.querySelector("nav");
+console.log(nav);
+
+burger.addEventListener("click", function(){
+    burger.classList.toggle("active");
+    nav.classList.toggle("active");
+})
